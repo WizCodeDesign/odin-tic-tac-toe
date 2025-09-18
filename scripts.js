248e85f6d5b680f352bd731ad4d0142367d8548e
WizCodeDesign <182226633+WizCodeDesign@users.noreply.github.com>
2025-09-18 17:38:14 +0300

    a new commit

--- scripts.js
+++ scripts.js
@@ -8,25 +8,3 @@
 //the game is a draw when no player has 3 symbols in a row and no more symbols can be placed
 //win condition is if cells 1,2,3 or 1,4,7 or 2,5,8 or 3,6,9 are filled with the same player symbol
 
-const game = {
-    player1: 'X',
-    player2: 'O',
-    score1: 0,
-    score2: 0,
-    turn: 1,
-    gameBoard: document.querySelector('.game-board'),
-    cells: ["document.querySelector('#cell-1')", "document.querySelector('#cell-2')", "document.querySelector('#cell-3')"
-        , "document.querySelector('#cell-4')", "document.querySelector('#cell-5')", "document.querySelector('#cell-6')",
-         "document.querySelector('#cell-7')", "document.querySelector('#cell-8')", "document.querySelector('#cell-9')"],
-    startGameBtn: document.querySelector('#start-game'),
-    resetGameBtn: document.querySelector('#reset-game'),
-        startGame: function () {},
-        resetGame: function () {
-            //change all innertext to empty string for each cell id in the cells array
-            this.cells.forEach(cell => cell.innerText = '');
-        },
-        checkScore: function () {},
-}
-document.addEventListener('DOMContentLoaded', function () {
-
-});
